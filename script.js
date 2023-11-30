457cbc913189f97647b680cc41e017b3cf77657d
dylanteo <s10258069@connect.np.edu.sg>
2023-11-30 20:22:19 +0800

    added cast page for mobile and desktop

--- script.js
+++ script.js
@@ -40,3 +40,33 @@ writinghead.addEventListener('click', function() {
         }
     }
 });
+
+
+var indexValue = 1;
+showgroup(1);
+function side_slide(e) {
+    if (typeof indexValue === 'undefined' || isNaN(indexValue)) {
+        console.error("indexValue is undefined or NaN, re-initializing to 0");
+        indexValue = 1;
+    }
+    console.log("Value received in side_slide: ", e);
+    console.log("Before updating, indexValue in side_slide:", indexValue);
+    indexValue+=e;
+    showgroup(indexValue)
+    console.log("Updated indexValue: ", indexValue);
+}
+function showgroup(e){
+    
+    const group = document.querySelectorAll('.cast-group');
+    console.log("indexValue in showImg:", indexValue);
+    if(e > group.length){
+        indexValue = 1
+    }
+    else if(e<1){
+        indexValue = group.length
+    }
+    for(let i=0; i<group.length;i++){
+        group[i].style.display = "none";
+    }
+    group[indexValue-1].style.display = "flex"
+}
