593c531bb6e38a74846d0bc86fbd662dc3240782
dylanteo <s10258069@connect.np.edu.sg>
2023-12-01 21:40:02 +0800

    added plot page, moved items into folder

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// JavaScript code for interactive elements like dropdowns and sliders 
+
+// Dropdown function for about page
+const developmenthead = document.getElementById('developmenthead'); 
+const development = document.getElementById('development');
+
+const mediaQuery = window.matchMedia("(max-width: 768px)")
+developmenthead.addEventListener('click', function() { // Toggle function
+    if (mediaQuery.matches){
+        if (development.style.display === 'none') {
+            development.style.display = 'block';
+        }else {
+            development.style.display = 'none';
+        }
+    }
+});
+
+const writinghead = document.getElementById('writinghead');
+const writing = document.getElementById('writing');
+
+writinghead.addEventListener('click', function() {
+    if (mediaQuery.matches){
+        if (writing.style.display === 'none') {
+            writing.style.display = 'block';
+        }else {
+            writing.style.display = 'none';
+        }
+    }
+});
+
+
+
+// Sliding function for cast and plot pages
+var indexValue = 1;
+showgroup(1);
+function side_slide(e) {
+    if (typeof indexValue === 'undefined' || isNaN(indexValue)) { // Re defining indexValue if undefined as I had issues with it
+        console.error("indexValue is undefined or NaN, re-initializing to 0");
+        indexValue = 1;
+    }
+    console.log("Value received in side_slide: ", e);
+    console.log("Before updating, indexValue in side_slide:", indexValue);
+    indexValue+=e;
+    showgroup(indexValue)
+    console.log("Updated indexValue: ", indexValue);
+}
+function showgroup(e){
+    
+    const group = document.querySelectorAll('.cast-group');
+    console.log("indexValue in showImg:", indexValue);
+    if(e > group.length){
+        indexValue = 1
+    }
+    else if(e<1){
+        indexValue = group.length
+    }
+    for(let i=0; i<group.length;i++){
+        group[i].style.display = "none";
+    }
+    group[indexValue-1].style.display = "flex"
+}
+
+var indexValue = 1;
+showplot(1);
+function side_slide1(e) {
+    if (typeof indexValue === 'undefined' || isNaN(indexValue)) {
+        console.error("indexValue is undefined or NaN, re-initializing to 0");
+        indexValue = 1;
+    }
+    console.log("Value received in side_slide: ", e);
+    console.log("Before updating, indexValue in side_slide:", indexValue);
+    indexValue+=e;
+    showplot(indexValue)
+    console.log("Updated indexValue: ", indexValue);
+}
+function showplot(e){
+    
+    const section = document.querySelectorAll('.plot-section');
+    console.log("indexValue in showImg:", indexValue);
+    if(e > section.length){
+        indexValue = 1
+    }
+    else if(e<1){
+        indexValue = section.length
+    }
+    for(let i=0; i<section.length;i++){
+        section[i].style.display = "none";
+    }
+    section[indexValue-1].style.display = "flex"
+}
